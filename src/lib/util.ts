6256424f3c884c574ce93cb2ca7e594d1ebc4515
liuweigl <wei2460@qq.com>
2021-03-30 22:41:09 +0800

    chore: add comments for util function

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -3,10 +3,19 @@ import path from 'path'
 
 import { PLUGIN_DATA_DIR } from './constant'
 
+/**
+ * Resolve file path with `PLUGIN_DATA_DIR`
+ *
+ * @param fileName file name
+ * @returns absolute path
+ */
 export const resolvePath = (fileName: string) => {
   return path.resolve(PLUGIN_DATA_DIR, fileName)
 }
 
+/**
+ * Make sure `PLUGIN_DATA_DIR` exists
+ */
 export const ensureDataDir = async () => {
   try {
     await fs.promises.access(PLUGIN_DATA_DIR, fs.constants.F_OK)
@@ -15,6 +24,13 @@ export const ensureDataDir = async () => {
   }
 }
 
+/**
+ * Write data to the file under `PLUGIN_DATA_DIR`
+ *
+ * @param fileName file name
+ * @param data file content
+ * @returns file path
+ */
 export const writeFile = async (
   fileName: string,
   data: string | Uint8Array
@@ -25,6 +41,12 @@ export const writeFile = async (
   return filePath
 }
 
+/**
+ * Read content from the file under `PLUGIN_DATA_DIR`
+ *
+ * @param fileName file name
+ * @returns file content
+ */
 export const readFile = async (fileName: string) => {
   const filePath = resolvePath(fileName)
   await ensureDataDir()
