b275f9af6142cbe89c7d2a26b11fb55382a9abc8
liuweigl <wei2460@qq.com>
2021-04-02 22:42:57 +0800

    build: use tsc compile

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import plugin from './plugin'
+
+const viteCertificate = plugin
+
+export default viteCertificate
--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,4 +1,4 @@
-import { Plugin } from 'vite'
+import { createLogger, Plugin } from 'vite'
 
 import { PLUGIN_NAME } from '../lib/constant'
 import { getLocalV4Ips } from '../lib/util'
@@ -10,23 +10,30 @@ const plugin = (options: ViteCertificateOptions): Plugin => {
   return {
     name: PLUGIN_NAME,
     config: async config => {
-      if (config.server?.https) {
-        const ips = getLocalV4Ips()
-        const mkcert = Mkcert.create(options)
-        await mkcert.init()
-        const certificates = await mkcert.install([...ips, 'localhost'])
-        const keys = certificates.map(item => item.key)
-        const certs = certificates.map(item => item.cert)
-        if (config.server.https === true) {
-          config.server.https = {
+      if (!config.server?.https) {
+        return
+      }
+
+      const { logLevel } = config
+      const logger = createLogger(logLevel)
+      const ips = getLocalV4Ips()
+      const mkcert = Mkcert.create({
+        logger,
+        ...options
+      })
+
+      await mkcert.init()
+
+      const certificates = await mkcert.install([...ips, 'localhost'])
+      const keys = certificates.map(item => item.key)
+      const certs = certificates.map(item => item.cert)
+
+      return {
+        server: {
+          https: {
             key: keys,
             cert: certs
           }
-        } else {
-          Object.assign(config.server.https, {
-            key: keys,
-            cert: certs
-          })
         }
       }
     }
